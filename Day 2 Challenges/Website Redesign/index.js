c7c6411a9051562eef410c66853e0472b13e8eb1
ritikasarkar18 <ritikasarkar438@gmail.com>
2021-01-12 22:17:00 +0530

    BBC basic clone

---
+++ Day 2 Challenges/Website Redesign/index.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+ 
+    $("#owl-demo").owlCarousel({
+         
+        autoPlay: 3000, //Set AutoPlay to 3 seconds
+         navigation:false,
+        
+        items : 3,
+        itemsDesktop : [1199,3],
+        itemsDesktopSmall : [979,3],
+        
+    });
+  
+     
+   
+  });
